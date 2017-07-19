e02bbbfe9ac1e23ab11b792fbe36662a52b1e86a
basvanopheusden <svo213@nyu.edu>
2017-07-19 16:42:48 +0200

    brought player back

--- static/js/game.js
+++ static/js/game.js
@@ -155,3 +155,25 @@ function Board() {
 		}
 	}
 }
+
+-function Player() {
+	this.initials = $("#nameInputField").val();
+	this.last_initials = this.initials;
+	this.color = 0
+	this.score = 0
+	this.opponent_score = 0
+	this.game_index = 0
+	this.scorebox = $('#leftScorebox')
+	this.scoretext = $('#leftScorebox p')
+	this.opponent_scorebox = $('#rightScorebox')
+	this.opponent_scoretext = $('#rightScorebox p')
+	this.opponent_color = 1
+	this.opponent_initials = "QQ"
+	this.show_name = $("#leftScorebox h2").text(this.initials);
+	this.move = 99
+	this.move_start = 0
+	this.move_end = 0
+	this.duration = 0
+	this.mouse_t = [];
+	this.mouse_x = [];
+}
