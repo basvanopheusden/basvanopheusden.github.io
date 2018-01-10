9f9dc8faea0f904e82441bbd74b5fe85a98b2a1f
Christopher Fonseka <chris.fonseka@googlemail.com>
2018-01-10 16:41:33 +0000

    added a moveResponse struct to capture wait_time from the decision tree output

--- static/scripts/Code/main.cpp
+++ static/scripts/Code/main.cpp
@@ -4,29 +4,50 @@
 #include <ctime>
 using namespace std;
 
+struct moveResponse
+{
+    int index;
+    int wait_time;
+};
+
+enum gameEndResponse { win, draw, none };
+
 extern "C" {
-
-int makemove(int seed, char* bp, char* wp, bool player){
-  heuristic h;
-  zet m;
-  int wait_time;
-  mt19937_64 generator;
-  board b(binstringtouint64(bp),binstringtouint64(wp));
-  generator.seed(seed);
-  h.seed_generator(generator);
-  m=h.makemove_bfs(b,player);
-  cout<<bp<<"\t"<<wp<<endl;
-  wait_time=(h.iterations==0?2500:((4000*sqrt(h.iterations*h.gamma)+500)));
-  b.write(m);
-  b.add(m);
-
-  /*cout<<uint64totile(m.zet_id)<<endl<<uint64tobinstring(b.pieces[BLACK])<<endl<<uint64tobinstring(b.pieces[WHITE])<<endl;
-  if(is_win(b.pieces[player]))
-    cout<<"win"<<endl;
-  else if(b.is_full())
-    cout<<"draw"<<endl;
-  else cout<<"playing"<<endl;*/
-  return uint64totile(m.zet_id);
+    
+    moveResponse makemoveresponse(int seed, char* bp, char* wp, bool player) {
+        
+        moveResponse output;
+        heuristic h;
+        zet m;
+        mt19937_64 generator;
+        board b(binstringtouint64(bp),binstringtouint64(wp));
+        
+        generator.seed(seed);
+        h.seed_generator(generator);
+        m = h.makemove_bfs(b,player);
+        
+        cout<<bp<<"\t"<<wp<<endl;
+        
+        output.wait_time = (h.iterations == 0 ? 2500 : ((4000 * sqrt(h.iterations * h.gamma ) + 500)));
+        b.write(m);
+        b.add(m);
+        
+        /*
+         cout<<uint64totile(m.zet_id)<<endl<<uint64tobinstring(b.pieces[BLACK])<<endl<<uint64tobinstring(b.pieces[WHITE])<<endl;
+         if(is_win(b.pieces[player]))
+         cout<<"win"<<endl;
+         else if(b.is_full())
+         cout<<"draw"<<endl;
+         else cout<<"playing"<<endl;
+         */
+        
+        output.index = uint64totile(m.zet_id);
+        return output;
+    }
+    
+    int makemove (int seed, char* bp, char* wp, bool player) {
+        return makemoveresponse(seed, bp, wp, player).index;
+    }
 }
 
 }
